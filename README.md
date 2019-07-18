5c8eae83a609be7aa35a9e92396a4a0acc5d72d9
Son Chau <soncqb@gmail.com>
2019-07-18 13:59:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tiktok_ui_clone
+ 
---
+++ android/app/src/main/java/com/sonchau/tiktok_ui_clone/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sonchau.tiktok_ui_clone;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
