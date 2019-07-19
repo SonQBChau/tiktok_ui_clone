5543bc03b65fb19b874b45eb4ce2c0b96efd0b36
Son Chau <soncqb@gmail.com>
2019-07-19 13:14:20 -0500

    done layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_ui_clone/home.dart';
 import 'package:tiktok_ui_clone/widgets/actions_toolbar.dart';
 import 'package:tiktok_ui_clone/widgets/bottom_toolbar.dart';
 import 'package:tiktok_ui_clone/widgets/video_description.dart';
@@ -20,44 +21,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  Widget get bottomSection => Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: List<Widget>.generate(5, (index) => Container(height: 40.0, width: 40, color: Colors.purple[300])),
-      );
 
-  Widget get middleSection {
-    return Expanded(
-        child: Row(mainAxisSize: MainAxisSize.max, crossAxisAlignment: CrossAxisAlignment.end, children: <Widget>[
-      VideoDescription(),
-      ActionsToolbar(),
-    ]));
-  }
-
-  Widget get topSection {
-    return Container(
-      height: 100.0,
-      padding: EdgeInsets.only(bottom: 15.0),
-      color: Colors.yellow[300],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        children: <Widget>[
-          // Top section
-          topSection,
-
-          // Middle expanded
-          middleSection,
-
-          // Bottom Section
-          BottomToolbar(),
-        ],
-      ),
-    );
-  }
-}
